374462986a326118721f5c04f3219bb92f3fef20
尹全旺 <m17602144093@163.com>
2018-10-18 14:43:27 +0800

    format

--- src/pages/reports/DebugReport.vue
+++ src/pages/reports/DebugReport.vue
@@ -111,27 +111,7 @@
                 :header-cell-style="{textAlign:'center', background: '#F8F8FA'}"
                 :cell-style="{textAlign:'center'}"
             >
-                <el-table-column
-                    type="expand"
-                >
-                    <template slot-scope="props">
-                        <el-tabs>
-                            <el-tab-pane label="Request">
-                                <pre class="code-block" v-html="props.row.meta_data.request"></pre>
-                            </el-tab-pane>
-                            <el-tab-pane label="Response">
-                                <pre class="code-block" v-html="props.row.meta_data.response"></pre>
-                            </el-tab-pane>
-                            <el-tab-pane label="Validators" v-if="props.row.meta_data.validators.length !== 0">
-                                <pre class="code-block" v-html="props.row.meta_data.validators"></pre>
-                            </el-tab-pane>
-                            <el-tab-pane label="Attachment" v-if="props.row.attachment !== ''">
-                                <pre class="code-block" v-html="props.row.attachment"></pre>
-                            </el-tab-pane>
-                        </el-tabs>
 
-                    </template>
-                </el-table-column>
 
                 <el-table-column
                     label="名 称"
@@ -156,7 +136,7 @@
                     width="100"
                 >
                     <template slot-scope="scope">
-                        <span>{{ scope.row.meta_data.request.method }}</span>
+                        <span :class="scope.row.meta_data.request.method">{{ scope.row.meta_data.request.method }}</span>
                     </template>
                 </el-table-column>
 
@@ -177,128 +157,31 @@
                     </template>
                 </el-table-column>
 
+                <el-table-column
+                    type="expand"
+                    fixed
+                >
+                    <template slot-scope="props">
+                        <el-tabs>
+                            <el-tab-pane label="Request">
+                                <pre class="code-block" v-html="handleRequest(props.row.meta_data.request)"></pre>
+                            </el-tab-pane>
+                            <el-tab-pane label="Response">
+                                <pre class="code-block" v-html="handleResponse(props.row.meta_data.response)"></pre>
+                            </el-tab-pane>
+                            <el-tab-pane label="Validators" v-if="props.row.meta_data.validators.length !== 0">
+                                <pre class="code-block" v-html="props.row.meta_data.validators"></pre>
+                            </el-tab-pane>
+                            <el-tab-pane label="Attachment" v-if="props.row.attachment !== ''">
+                                <pre class="code-block" v-html="props.row.attachment"></pre>
+                            </el-tab-pane>
+                        </el-tabs>
+
+                    </template>
+                </el-table-column>
+
             </el-table>
         </slot>
-        <!--<div
-            class="popup"
-            v-for="test_suite in summary.details"
-        >
-            <div
-                class="content"
-                v-for="item in test_suite.records"
-            >
-                <div style="text-align: center; font-size: 20px; color: deepskyblue; margin-top: 20px">
-                    {{item.name}} &nbsp;<span :class="item.status">{{item.status}}</span>
-                </div>
-
-                <h3 class="title">Request:</h3>
-                <div style="overflow: auto">
-                    <table class="details">
-                        <tr v-for="(value, key) in item.meta_data.request">
-                            <slot v-if="value !== null">
-                                <th>{{ key }}</th>
-                                <td>
-                                <span v-if="key === 'headers' ">
-                                    <div
-                                        v-for="(header_value, header_key) in item.meta_data.request.headers">
-                                       {{ header_key }}: &nbsp;{{ header_value }}
-                                    </div>
-                                </span>
-                                <span v-else>
-                                {{ value }}
-                                </span>
-                                </td>
-                            </slot>
-                        </tr>
-                    </table>
-                </div>
-
-                <h3 class="title">Response:</h3>
-                <div style="overflow: auto">
-                    <table class="details">
-                        <tr
-                            v-if='["text", "json", "elapsed_ms", "response_time_ms", "content_size", "content_type"].indexOf(key) === -1'
-                            v-for="(value, key) in item.meta_data.response"
-                        >
-                            <slot v-if="value !== null">
-                                <th>{{ key }}</th>
-                                <td>
-                                <span v-if="key === 'headers' ">
-                                    <div
-                                        v-for="(header_value, header_key) in item.meta_data.response.headers">
-                                    {{ header_key }}: &nbsp;{{ header_value }}
-                                    </div>
-                                </span>
-                                    <span v-else-if="key === 'content' ">
-                                    <img
-                                        v-if="item.meta_data.response.content_type.indexOf('image') !== -1 "
-                                        :src="item.meta_data.response.content"
-                                    />
-                                    <pre v-else>{{item.meta_data.response.text}}</pre>
-                                </span>
-
-                                <span v-else-if="key === 'cookies' ">
-                                    <div
-                                        v-for="(cookie_value, cookie_key) in item.meta_data.response.cookies">
-                                       {{ cookie_key }}: &nbsp;{{ cookie_value }}
-                                    </div>
-                                </span>
-
-                                <span v-else>{{value}}</span>
-
-                                </td>
-                            </slot>
-                        </tr>
-                    </table>
-                </div>
-
-                <h3 class="title">Validators:</h3>
-                <div style="overflow: auto">
-                    <table class="details">
-                        <tr>
-                            <th>check</th>
-                            <th>comparator</th>
-                            <th>expect value</th>
-                            <th>actual value</th>
-                        </tr>
-                        <tr v-for="validator in item.meta_data.validators">
-                            <td class="passed" v-if="validator.check_result === 'pass' ">
-                            <td class="failed" v-if="validator.check_result === 'fail' ">
-                            <td class="unchecked" v-if="validator.check_result === 'unchecked' ">
-                                {{ validator.check}}
-                            </td>
-                            <td>{{ validator.comparator }}</td>
-                            <td>{{ validator.expect }}</td>
-                            <td>{{ validator.check_value }}</td>
-                        </tr>
-                    </table>
-                </div>
-
-                <h3 class="title">Statistics:</h3>
-                <div style="overflow: auto">
-                    <table class="details">
-                        <tr>
-                            <th>content_size(bytes)</th>
-                            <td>{{ item.meta_data.response.content_size }}</td>
-                        </tr>
-                        <tr>
-                            <th>response_time(ms)</th>
-                            <td>{{ item.meta_data.response.response_time_ms }}</td>
-                        </tr>
-                        <tr>
-                            <th>elapsed(ms)</th>
-                            <td>{{ item.meta_data.response.elapsed_ms }}</td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div class="content" v-if="item.attachment">
-                    <h3 class="title">Exceptions</h3>
-                    <pre class="code-block">{{item.attachment}}</pre>
-                </div>
-            </div>
-
-        </div>-->
     </div>
 
 </template>
@@ -306,6 +189,20 @@
 <script>
     export default {
         name: "DebugReport",
+        methods: {
+            handleRequest (request) {
+                const keys = ["start_timestamp"];
+                keys.forEach(function(item) {delete request[item]});
+                return request
+            },
+
+            handleResponse (response) {
+                const keys = ["response_time_ms", "encoding", "ok", "reason", "url", "text", "json", "content_size", "content_type"];
+                keys.forEach(function(item) {delete response[item]});
+                try {response["content"] = JSON.parse(response["content"]);} catch (err) {}
+                return response
+            }
+        },
         props: {
             summary: {
                 require: true
